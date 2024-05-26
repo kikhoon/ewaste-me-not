24750cb44052a2629994022cb7da15f093471d93
kikhoon <131383408+kikhoon@users.noreply.github.com>
2024-05-26 16:54:28 +0000

    Adapted further

--- prisma/migrations/20240324115353_/migration.sql
+++ prisma/migrations/20240324115353_/migration.sql
@@ -6,3 +6,25 @@ CREATE TABLE "Post" (
 
     CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
 );
+
+CREATE TABLE "Bin" (
+    "id" SERIAL NOT NULL,
+    "wkt" TEXT NOT NULL,
+    "servicepointname" TEXT NOT NULL,
+    "lat" FLOAT NOT NULL,
+    "long" FLOAT NOT NULL,
+    "location" TEXT NOT NULL,
+    "postalcode" BIGINT NOT NULL,
+    "ictequipment" BOOLEAN NOT NULL,
+    "batteries" BOOLEAN NOT NULL,
+    "lamps" BOOLEAN NOT NULL,
+    "smallhouseholdappliances" BOOLEAN NOT NULL,
+    "gamingconsoles" BOOLEAN NOT NULL,
+    "audiosystems" BOOLEAN NOT NULL,
+    "powersupplies" BOOLEAN NOT NULL,
+    "website" TEXT NOT NULL,
+    "premisestype" TEXT NOT NULL,
+    "dateupdated" BIGINT NOT NULL,
+ 
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
